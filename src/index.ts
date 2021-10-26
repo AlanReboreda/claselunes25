6d0d9ab3e4c5838d66a57be18a785b9be934348b
AlanReboreda <92063942+AlanReboreda@users.noreply.github.com>
2021-10-25 21:37:13 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+function calcularbasetriangulo(base: number, altura: number) : number{
+  let area : number = base * altura /2;
+
+  return area;
+}
+ for(let i: number = 1; i <=7; i++){
+   console.log(calcularbasetriangulo(i; i*2));
+ }
+
+
